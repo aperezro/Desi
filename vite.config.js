08e8e046eab071b0591c7572789a7e164bb925ff
Alejandro <aperezro@iastate.edu>
2025-09-20 19:09:49 -0500

    custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Desi/',           // repo path (case-sensitive)
+  base: '/',           // repo path (case-sensitive)
   plugins: [tailwindcss(), react()],
 })
